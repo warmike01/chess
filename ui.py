ead5f8d9bd0cf1d8539487dbc912ce87b6d41be0
warmike01 <warmike01@gmail.com>
2023-05-21 00:43:25 +0300

    отдельная папка для иконок

--- ui.py
+++ ui.py
@@ -8,9 +8,9 @@ def square_pic(b: chess.board, i: int, j: int):
     square = b.board_state[i][j]
     color = (i + j) %2
     if square.s_piece !=0:
-        return "{0}_{1}_{2}.PNG".format(square.s_piece, square.s_color, color)
+        return "resources/{0}_{1}_{2}.PNG".format(square.s_piece, square.s_color, color)
     else:
-        return "0_{0}.PNG".format(color)
+        return "resources/0_{0}.PNG".format(color)
 board_gfx=[]
 pics=[]
 def board_update(b: chess.board):
